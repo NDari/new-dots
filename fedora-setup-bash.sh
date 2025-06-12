7cd456e53ffeb3e8a85b4238201bde649203cd23
Naseer Dari <naseerdari01@gmail.com>
2025-06-11 19:51:52 -0500

    add bash

---
+++ fedora-setup-bash.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -eu pipefail
+
+cd $HOME
+
+# git
+sudo dnf install -y git
+
+git clone https://github.com/NDari/new-dots.git $HOME/dotfiles
+
+# git conf
+git config --global init.defaultbranch "main"
+git config --global core.whitespace "cr-at-eol"
+git config --global include.path $HOME/dotfiles/delta/delta.conf
+
+# zsh
+cd $HOME
+ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
+
+# nvim
+cd $HOME
+mkdir -p $HOME/.config/nvim
+ln -s $HOME/dotfiles/nvim/init.lua $HOME/.config/nvim/init.lua
+
+mkdir $HOME/.ssh
+
+sudo dnf install -y \
+	fzf \
+	neovim \
+	direnv \
+	tmux \
+	xclip \
+	openssl-devel \
+	uv
+
+# tmux
+cd $HOME
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+$HOME/.tmux/plugins/tpm/bin/install_plugins
+
+# go
+GO_VERSION="go1.24.4"
+mkdir -p $HOME/tools && cd $HOME/tools
+curl -LO https://go.dev/dl/${GO_VERSION}.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${GO_VERSION}.linux-amd64.tar.gz
+rm ${GO_VERSION}.linux-amd64.tar.gz
+cd $HOME
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+$HOME/.cargo/bin/cargo install cargo-update
+$HOME/.cargo/bin/cargo install du-dust
+$HOME/.cargo/bin/cargo install git-delta
+$HOME/.cargo/bin/cargo install just
+$HOME/.cargo/bin/cargo install fd-find
+$HOME/.cargo/bin/cargo install ripgrep
+$HOME/.cargo/bin/cargo install bat
+$HOME/.cargo/bin/cargo install zoxide
+$HOME/.cargo/bin/cargo install procs
+$HOME/.cargo/bin/cargo install tealdeer
+
+# to remove windows $PATH from infecting this path in WSL
+# will need to shut down and restart (wsl --shutdown)
+if [[ -f /etc/wsl.conf ]]
+then
+	sudo bash -c "cat <<EOF>> /etc/wsl.conf
+
+[interop]
+appendWindowsPath = false
+EOF
+"
+fi
